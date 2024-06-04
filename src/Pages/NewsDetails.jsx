2410af4843bfe662127b89967649ee0f3e1011c9
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-06-04 07:54:19 +0100

    newsDetails

--- src/Pages/NewsDetails.jsx
+++ src/Pages/NewsDetails.jsx
@@ -1,27 +1,40 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import Header from "../shared/Header";
 import Navbar from "../shared/Navbar";
 import RightSideNav from "../shared/RightSideNav";
 
-
 const NewsDetails = () => {
-    const {id} = useParams()
-    
-    return (
-        <div>
-            <Header></Header>
-            <Navbar></Navbar>
-            <div className="grid grid-cols-1 lg:grid-cols-4">
-                <div className="lg:col-span-3">
-                    <h1>NesDetails {id}</h1>
-                </div>
-                <div>
-                    <RightSideNav></RightSideNav>
-                </div>
-            </div>
+  const { id } = useParams();
+  const news = useLoaderData();
+  const newsD = news.find((aNews) => aNews._id == id);
+  console.log(newsD);
+  const {details,image_url,title} = newsD
 
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <div className="grid grid-cols-1 lg:grid-cols-4">
+        <div className="lg:col-span-3">
+          <div className="card bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src={image_url}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{title}</h2>
+              <p>{details}</p>
+            </div>
+          </div>
         </div>
-    );
+        <div>
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewsDetails;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -21,7 +21,8 @@ const router = createBrowserRouter([
         },
         {
             path: "/news/:id",
-            element : <PrivateRoute><NewsDetails></NewsDetails></PrivateRoute>
+            element : <PrivateRoute><NewsDetails></NewsDetails></PrivateRoute>,
+            loader : () => fetch('news.json')
         },
         {
           path : "/login",
