b33f4bef0777a1934afd3dac6418f32ee5095175
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-06-04 07:41:41 +0100

    firebase make private..

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDoKruSCfxk62wL0lTeN1OQXYQyXRRKo7c",
-  authDomain: "newspaper-9aefe.firebaseapp.com",
-  projectId: "newspaper-9aefe",
-  storageBucket: "newspaper-9aefe.appspot.com",
-  messagingSenderId: "872015882375",
-  appId: "1:872015882375:web:f0dd720e227408c6a87022"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
